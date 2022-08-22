c54b915209ef70dc2a76393d6c072dcf99381e87
M.Shahzaib <shahzaibmuhammad627.com>
2022-08-22 04:54:10 -0700

    i done it

--- src/components/cardwrite/Cardwrite.js
+++ src/components/cardwrite/Cardwrite.js
@@ -1,72 +1,78 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import thumbnail from '../images/thumbnail.png'
 import { Para } from '../Stylecomp'
 
 
 const cardapi = [
     {
-        thumbnail : thumbnail,
-        heading : 'Yuga Labs Acquires CryptoPunks and Meebits',
-        para : 'Yuga Labs has acquired the IP of the CryptoPunks and Meebits,will transfer IP rights to individual token holders.'
+        thumbnail: thumbnail,
+        heading: 'Yuga Labs Acquires CryptoPunks and Meebits',
+        para: 'Yuga Labs has acquired the IP of the CryptoPunks and Meebits,will transfer IP rights to individual token holders.'
     },
     {
-        thumbnail : thumbnail,
-        heading : 'Yuga Labs Acquires CryptoPunks and Meebits',
-        para : 'Yuga Labs has acquired the IP of the CryptoPunks and Meebits,will transfer IP rights to individual token holders.'
+        thumbnail: thumbnail,
+        heading: 'Yuga Labs Acquires CryptoPunks and Meebits',
+        para: 'Yuga Labs has acquired the IP of the CryptoPunks and Meebits,will transfer IP rights to individual token holders.'
     },
     {
-        thumbnail : thumbnail,
-        heading : 'Yuga Labs Acquires CryptoPunks and Meebits',
-        para : 'Yuga Labs has acquired the IP of the CryptoPunks and Meebits,will transfer IP rights to individual token holders.'
+        thumbnail: thumbnail,
+        heading: 'Yuga Labs Acquires CryptoPunks and Meebits',
+        para: 'Yuga Labs has acquired the IP of the CryptoPunks and Meebits,will transfer IP rights to individual token holders.'
     },
     {
-        thumbnail : thumbnail,
-        heading : 'Yuga Labs Acquires CryptoPunks and Meebits',
-        para : 'Yuga Labs has acquired the IP of the CryptoPunks and Meebits,will transfer IP rights to individual token holders.'
+        thumbnail: thumbnail,
+        heading: 'Yuga Labs Acquires CryptoPunks and Meebits',
+        para: 'Yuga Labs has acquired the IP of the CryptoPunks and Meebits,will transfer IP rights to individual token holders.'
     },
     {
-        thumbnail : thumbnail,
-        heading : 'Yuga Labs Acquires CryptoPunks and Meebits',
-        para : 'Yuga Labs has acquired the IP of the CryptoPunks and Meebits,will transfer IP rights to individual token holders.'
+        thumbnail: thumbnail,
+        heading: 'Yuga Labs Acquires CryptoPunks and Meebits',
+        para: 'Yuga Labs has acquired the IP of the CryptoPunks and Meebits,will transfer IP rights to individual token holders.'
     },
     {
-        thumbnail : thumbnail,
-        heading : 'Yuga Labs Acquires CryptoPunks and Meebits',
-        para : 'Yuga Labs has acquired the IP of the CryptoPunks and Meebits,will transfer IP rights to individual token holders.'
+        thumbnail: thumbnail,
+        heading: 'Yuga Labs Acquires CryptoPunks and Meebits',
+        para: 'Yuga Labs has acquired the IP of the CryptoPunks and Meebits,will transfer IP rights to individual token holders.'
     }
 ]
 
 const Cardwrite = () => {
+    const navigate=useNavigate()
+    const [cards, setcards] = useState(cardapi)
 
-        const [cards,setcards] = useState(cardapi)
-
-  return (
-   <>
-<div className="mega-card-container">
+    function handleSubmit() {
+        navigate('../blog', { replace:  true })
+    }
 
 
-   {
-    cards.map((prop) =>{
+    return (
+        <>
+            <div className="mega-card-container">
 
-         const {thumbnail,heading,para} = prop;
-        return(
-            <>
-            
-             <div class="card-container">
-            <img src={thumbnail} alt="Avatar" style={{width:'100%'}} />
-            <div class="card-content">
-            <h4><b>{heading}</b></h4> 
-              <p>{para}</p> 
+
+                {
+                    cards.map((prop) => {
+
+                        const { thumbnail, heading, para } = prop;
+                        return (
+                            <>
+
+                                <div class="card-container" onClick={handleSubmit}>
+                                    <img src={thumbnail} alt="Avatar" style={{ width: '100%' }} />
+                                    <div class="card-content">
+                                        <h4><b>{heading}</b></h4>
+                                        <p>{para}</p>
+                                    </div>
+                                </div>
+
+                            </>
+                        )
+                    })
+                }
+
             </div>
-            </div>
-             </>
-        )
-    })
-   }
-  
-    </div>
-   </>
+        </>
     )
 }
 
